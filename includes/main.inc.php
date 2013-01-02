1260c6208a450605693ba506b218aad62a69c4c4
Jasper Vries <jasper.vries@denhaag.nl>
2013-01-02 14:14:58 +0100

    Feature: main page

---
+++ includes/main.inc.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * Gemeente Den Haag, Dienst Stadsbeheer, Afdeling Verkeersmanagement en Openbare Verlichting, 2013
+*/
+?>
+
+<h1>Op dit moment</h1>
+<h2>Incidenten</h2>
+<p><a href="?p=i">nieuw</a> | <a href="?p=i_hist">historie</a></p>
+
+<?php
+$qry = "SELECT `id`, `date` 
+	FROM `".$sql['database']."`.`".$sql['table_i']."`
+	WHERE `open` = 1
+	ORDER BY `date` DESC";
+$res = mysqli_query($sql['link'], $qry);
+if (mysqli_num_rows($res)) {
+echo '<table>';
+	while ($row = mysqli_fetch_row($res)) {
+		echo '<tr><td>'.$row[1].'</td>';
+		$qry2 = "SELECT `time`, `description`
+		FROM `".$sql['database']."`.`".$sql['table_id']."`
+		WHERE `parent_id` = ".$row[0]."
+		ORDER BY `time`";
+		$res2 = mysqli_query($sql['link'], $qry2);
+		if (mysqli_num_rows($res2)) {
+			while ($row2 = mysqli_fetch_row($res2)) {
+				//nog iets toevoegen van string afbreken na bepaalde lengte
+				echo '<td>'.$row[0].'</td><td>'.htmlspecialchars(str_replace('<br />', ' ', $row[1])).'</td></tr><tr><td>&nbsp;</td>';
+			}
+		}
+		echo '<td>&nbsp;</td><td>&nbsp;</td></tr>';
+	}
+echo '</table>';
+}
+else {
+	?>
+	<p>Geen openstaande incidenten.</p>
+	<?php
+}
+?>
+
+<h2>Werkzaamheden</h2>
+<p><a href="?p=w">nieuw</a> | <a href="?p=w_hist">historie</a></p>
+
+<?php
+$qry = "SELECT `id`, `datetime_start`, `datetime_end`, `description` 
+	FROM `".$sql['database']."`.`".$sql['table_w']."`
+	WHERE `datetime_end` > NOW()
+	ORDER BY `datetime_start`";
+$res = mysqli_query($sql['link'], $qry);
+if (mysqli_num_rows($res)) {
+echo '<table>';
+	while ($row = mysqli_fetch_row($res)) {
+		echo '<tr><td>'.$row[1].'</td><td>'.$row[2].'</td><td>'.htmlspecialchars($row[3]).'</td></tr>';
+	}
+echo '</table>';
+}
+else {
+	?>
+	<p>Geen werkzaamheden.</p>
+	<?php
+}
+?>
+
+<h2>Evenementen</h2>
+<p><a href="?p=e">nieuw</a> | <a href="?p=e_hist">historie</a></p>
+
+<?php
+$qry = "SELECT `id`, `datetime_start`, `datetime_end`, `description` 
+	FROM `".$sql['database']."`.`".$sql['table_e']."`
+	WHERE `datetime_end` > NOW()
+	ORDER BY `datetime_start`";
+$res = mysqli_query($sql['link'], $qry);
+if (mysqli_num_rows($res)) {
+echo '<table>';
+	while ($row = mysqli_fetch_row($res)) {
+		echo '<tr><td>'.$row[1].'</td><td>'.$row[2].'</td><td>'.htmlspecialchars($row[3]).'</td></tr>';
+	}
+echo '</table>';
+}
+else {
+	?>
+	<p>Geen werkzaamheden.</p>
+	<?php
+}
+?>
