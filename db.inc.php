f1e58a0cd42be5f861dddc7c751f462814f06dd6
Jasper Vries <jasper.vries@denhaag.nl>
2013-01-02 12:58:10 +0100

    Feature: install script

---
+++ db.inc.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Gemeente Den Haag, Dienst Stadsbeheer, Afdeling Verkeersmanagement en Openbare Verlichting, 2013
+*/
+
+//database credentials and settings
+$sql['server'] = 'localhost';
+$sql['database'] = 'ovm';
+$sql['table_prefix'] = 'ovm_';
+$sql['table_i'] = $sql['table_prefix'].'incidenten';
+$sql['table_id'] = $sql['table_prefix'].'incidenten_details';
+$sql['table_w'] = $sql['table_prefix'].'werkzaamheden';
+$sql['table_e'] = $sql['table_prefix'].'evenementen';
+$sql['user'] = 'root';
+$sql['password'] = '';
+?>
---
+++ install.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Gemeente Den Haag, Dienst Stadsbeheer, Afdeling Verkeersmanagement en Openbare Verlichting, 2013
+*/
+
+include('db.inc.php');
+
+//connect to database
+$sql['link'] = mysqli_connect($sql['server'], $sql['user'], $sql['password']);
+
+//create database
+$qry = "CREATE DATABASE IF NOT EXISTS `".$sql['database']."`
+		CHARACTER SET = latin1
+		COLLATE = latin1_general_ci";
+mysqli_query($sql['link'], $qry);
+echo mysqli_error($sql['link']);
+
+//create table "incidenten"
+$qry = "CREATE TABLE IF NOT EXISTS `".$sql['database']."`.`".$sql['table_i']."` (
+		`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		`date` DATE NOT NULL,
+		`road` TINYTEXT,
+		`location` TINYTEXT,
+		`scenario` TINYTEXT,
+		`open` BOOLEAN
+	) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci";
+mysqli_query($sql['link'], $qry);
+echo mysqli_error($sql['link']);
+
+//create table "incidenten_details"
+$qry = "CREATE TABLE IF NOT EXISTS `".$sql['database']."`.`".$sql['table_id']."` (
+		`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		`parent_id` INT UNSIGNED NOT NULL,
+		`time` TIME NOT NULL,
+		`description` TEXT,
+		`contact` TINYTEXT
+	) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci";
+mysqli_query($sql['link'], $qry);
+echo mysqli_error($sql['link']);
+
+//create table "werkzaamheden"
+$qry = "CREATE TABLE IF NOT EXISTS `".$sql['database']."`.`".$sql['table_w']."` (
+		`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		`datetime_start` DATETIME NOT NULL,
+		`datetime_end` DATETIME NOT NULL,
+		`road` TINYTEXT,
+		`location` TINYTEXT,
+		`description` TEXT,
+		`scenario` TINYTEXT
+	) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci";
+mysqli_query($sql['link'], $qry);
+echo mysqli_error($sql['link']);
+
+//create table "evenementen"
+$qry = "CREATE TABLE IF NOT EXISTS `".$sql['database']."`.`".$sql['table_e']."` (
+		`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		`datetime_start` DATETIME NOT NULL,
+		`datetime_end` DATETIME NOT NULL,
+		`road` TINYTEXT,
+		`location` TINYTEXT,
+		`description` TEXT,
+		`scenario` TINYTEXT
+	) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci";
+mysqli_query($sql['link'], $qry);
+echo mysqli_error($sql['link']);
+
+echo '<p>done</p>';
+?>
