b9761c13d557991a3623711e296b9c6a45700e49
Jasper Vries <jasper.vries@denhaag.nl>
2016-07-11 14:35:31 +0200

    Change: Jquery via CDN

--- index.php
+++ index.php
@@ -24,14 +24,14 @@ if (!empty($_GET['s']) && file_exists('includes/'.urlencode($_GET['s']).'_save.i
 <html>
 <head>
 <title>OVM logging systeem</title>
-<link rel="stylesheet" type="text/css" href="style.css" />
-<link rel="stylesheet" type="text/css" href="print.css" media="print" />
-<link rel="stylesheet" href="jquery/jquery-ui.css" />
-<script src="jquery/jquery-1.8.3.js"></script>
-<script src="jquery/jquery-ui.js"></script>
+<script src="//code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="//code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
 <script src="jquery/jquery.ui.datepicker-nl.js"></script>
 <script src="jquery/jquery-ui-timepicker-addon.js"></script>
 <script src="jquery/jquery-ui-timepicker-addon-nl.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="print.css" media="print" />
 </head>
 <body>
 
