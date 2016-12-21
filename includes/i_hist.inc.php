2296b3c20c93f93b2ac22177c29a19cc7ff1c4ed
Jasper Vries <jasper.vries@denhaag.nl>
2016-12-21 13:35:06 +0100

    Feature: paginatie in incidenten historie

--- includes/i_hist.inc.php
+++ includes/i_hist.inc.php
@@ -12,7 +12,27 @@ $qry = "SELECT `id`, `date`, `road`, `location`, `scenario`, `review`
 	FROM `".$sql['database']."`.`".$sql['table_i']."`
 	ORDER BY `date` DESC";
 $res = mysqli_query($sql['link'], $qry);
-if (mysqli_num_rows($res)) {
+$num = mysqli_num_rows($res);
+$step = 100;
+if (is_numeric($_GET['start'])) $start = $_GET['start']; else $start = 0;
+if ($start > $num) $start = $num - $step;
+
+if ($num) {
+	
+	//build pagination
+	$pag = '<p style="float:right;">'.($start+1).' tot '.min(($start+$step), $num).' van '.$num.'
+	| <a href="?p=i_hist&amp;start=0">&laquo; eerste</a>
+	| <a href="?p=i_hist&amp;start='.max(0, ($start - $step)).'">&lt; vorige</a>
+	| <a href="?p=i_hist&amp;start='.min((floor($num/$step)*$step), max(0, ($start + $step))).'">volgende &gt;</a>
+	| <a href="?p=i_hist&amp;start='.(floor($num/$step)*$step).'">laatste &raquo;</a></p>';
+	
+	$qry = "SELECT `id`, `date`, `road`, `location`, `scenario`, `review`  
+		FROM `".$sql['database']."`.`".$sql['table_i']."`
+		ORDER BY `date` DESC
+		LIMIT ".$start.",".$step;
+	$res = mysqli_query($sql['link'], $qry);
+	
+	echo $pag;
 	echo '<table class="grid">';
 	echo '<tr><th>datum</th><th>tijd</th><th>locatie/omschrijving</th><th>scenario</th><th>evaluatie</th></tr>';
 	while ($row = mysqli_fetch_row($res)) {
@@ -28,6 +48,7 @@ if (mysqli_num_rows($res)) {
 		echo '<tr><td>'.date('d-m-Y', strtotime($row[1])).'</td><td>'.date('H:i', strtotime($row2[0])).'</td><td class="expand"><a href="?p=i_view&amp;id='.$row[0].'">'.htmlspecialchars($row[2].' - '.$row[3]).'</a></td><td>'.htmlspecialchars($row[4]).'</td><td>'.(($row[5] == '1') ? 'Ja' : '').'</td></tr>';
 	}
 	echo '</table>';
+	echo $pag;
 }
 else {
 	?>
